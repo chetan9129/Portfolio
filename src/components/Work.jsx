4a070b66ee3de50bf5aee9e4c93dfb39c23ef314
Chetan Sharma <anku9129@gmail.com>
2022-08-02 09:54:52 +0530

    Minor Changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -7,7 +7,7 @@ import WorkImg3 from "../assests/webImage3.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-black bg-[#D6EFED]">
+    <div name="work" className="w-full md:h-screen text-black bg-[#m]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-black border-pink-600">
