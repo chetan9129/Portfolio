757837fc575448de58e84d81a28a89d4d578f819
Chetan Sharma <anku9129@gmail.com>
2022-08-08 13:33:55 +0530

    About Me Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,12 +9,12 @@ const Home = () => {
         <p className="text-[#EF5B0C] text-2xl">Hi, my name is </p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#003865]">Chetan Sharma</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#cfad3c]">
-          I am a Student and Full Stack Devloper
+          I am a Student and Front End Devloper.
         </h2>
         <p className="text-[#cfad3c] py-4 max-w-700 text-xl">
-          I'm a full stack devloper specialising in building ocassionally
+          I'm a Front-End Devloper specialising in building ocassionally
           designing exceptional digital experiences. Currently, I'm focused on
-          building responsive full-stack web application.
+          building responsive front-end web application.
         </p>
         <div>
           <button className="text-black group border-black border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:duration-500 hover:border-pink-600">View Work
